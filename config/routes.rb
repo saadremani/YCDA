74988f2502bebcfbd9b0aaf3b88935b88049514e
saadremani <remani.islam2014@gmail.com>
2019-12-07 22:59:08 +0100

    style working on Devise and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/secret'
-
-  get '/users', to: 'static#index'
 
   devise_for :users
+  resources :static_pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
