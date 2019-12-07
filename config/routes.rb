bfacea7a910918e06b1266ffc69517fa6c8ebba1
saadremani <remani.islam2014@gmail.com>
2019-12-07 20:34:40 +0100

    add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/secret'
+
+  get '/users', to: 'static#index'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
